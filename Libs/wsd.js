22ef0a4c721c4a7ee4a334349864cd4b1272714d
glitchFling <glitchfling378@gmail.com>
2026-07-09 15:21:03 -0500

    Update wsd.js

--- Libs/wsd.js
+++ Libs/wsd.js
@@ -3,3 +3,11 @@
 // Detects if the $text is only whitespaces;
 //
 // Spaces and Whitespaces.
+
+var input = (typeof $text !== 'undefined' && $text !== null) ? $text : "";
+
+function isWhiteSpaceOnly(str) {
+    return /^\s*$/.test(str);
+}
+
+return String(isWhiteSpaceOnly(input));
